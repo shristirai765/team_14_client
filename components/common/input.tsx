cf14acfcde53bba24d02a47b964620e9283b669e
Shristi Rai <shristirai.it@gmail.com>
2026-08-07 13:16:20 +0545

    add: hide and unhide password feature

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { UseFormRegister } from 'react-hook-form';
 import { FaAsterisk } from "react-icons/fa6";
+import { LuEyeClosed } from "react-icons/lu";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 interface IProps {
     label: string;
@@ -14,22 +16,40 @@ interface IProps {
 }
 
 const Input: FC<IProps> = ({ id, label, name, placeholder, type = 'text', register, error, required = false }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onClick = ()=>{
+        setShowPassword(!showPassword);
+    }
     return (
         <div className='flex flex-col gap-1'>
             <div className='flex gap-0.5'>
                 <label className='text-sm font-semibold' htmlFor={id}>{label}</label>
                 {required && <FaAsterisk size={10} className='text-primary mt-0.5' />}
+                {/* {type === "password" && <LuEyeClosed onClick={onClick} className=' size-3.5 mt-1'/>} */}
             </div>
 
-            <input
-                {...register(name)}
-                id={id}
-                placeholder={placeholder}
-                type={type}
-                className={`accent-primary py-3.5 border px-2 rounded-sm  text-sm placeholder:text-sm
-                    ${error ? 'border-red-500 focus:outline-red-500' : 'border-primary-light focus:outline-primary-active'} 
+           <div className={`flex items-end accent-primary border rounded-sm  text-sm placeholder:text-sm
+                        
                     `}
-            />
+            >
+                <input
+                    {...register(name)}
+                    id={id}
+                    placeholder={placeholder}
+                    className={`w-full border accent-primary py-3.5 px-2 rounded-sm  text-sm placeholder:text-sm placeholder-white-900
+                        ${error ? 'border-red-500 focus:outline-red-500' : 'border-primary-light focus:outline-primary-active'}`}
+                    type={!showPassword && type==="password" ? "password" : "text"}
+                    // type= {type}
+                    
+                />
+                <div className='size-5 p-auto'>
+                    {type === "password" && (showPassword ? <MdOutlineRemoveRedEye onClick={onClick}/> : <LuEyeClosed onClick={onClick}/>)}
+                    
+                </div>
+           </div>
+                
+            
             <small
                 className='h-5 text-red-500 text-xs'
             >{error}</small>
