e4e2351544353663f648b42349d9756b44697452
Shristi Rai <shristirai.it@gmail.com>
2026-08-21 16:17:43 +0545

    api base url

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create axios instance
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1/",
+    baseURL: "https://mern-project-server-ngeb.onrender.com/api/v1/",
     // headers:{
     //     "Content-Type": ""
     // }
